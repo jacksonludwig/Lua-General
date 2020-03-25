884843782f8dc474e74c4774cb9367cbed0cf023
ludwj61 <ludwj61@mail.sunysuffolk.edu>
2020-03-24 23:12:13 -0400

    add a lot

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+--https://learnxinyminutes.com/docs/lua/ IFFY SUMMARY
+--https://www.lua.org/pil/4.1.html ASSIGNMENT
+--https://www.lua.org/pil/4.2.html LOCAL VARS AND BLOCKS
+--https://www.lua.org/pil/4.3.html LOOPS
+--https://www.lua.org/pil/2.5.html TABLES
+--https://www.lua.org/pil/2.6.html FUNCTIONS
+
 num = 42
 
 a_string = "single quotes or double make immutable strings... only one liners"
@@ -19,7 +26,7 @@ elseif a_string == big_string then
 else
     thisIsGlobal = 5
 
-    local thisIsLocal = io.read()
+    local thisIsLocal = io.read() -- TRY TO DO THIS WHENEVER POSSIBLE, FASTER, SAFER
 
     print("String concatenation uses two dots " .. thisIsLocal)
 end
@@ -39,7 +46,7 @@ ans = aBool and "yes" or "no" --> no
 print()
 
 num2 = 0
--- beginning value, end value, step value
+-- beginning value, end value, step value (not needed if 1)
 for i = 0, 99, 1 do
     num2 = num2 + 1
 end
@@ -65,12 +72,100 @@ function adder(x)
         return x + y
     end
 end
+-- the following are the same:
+function f(x)
+    return x * x
+end
+f = function(x)
+    return x * x
+end
 
 x, y, z = 1, 2, 3, 4 -- 4 thrown out
 
-function giveMeAList(a, b, c)
+function giveMeAList(a, b, c) -- NOT REALLY A LIST: it returns multiple values!
     print(a, b, c) -- tabs between each value
     return 4, 8, 15, 16, 23, 42
 end
 x, y = giveMeAList("jackson") -- missing arguments are printed as nil values
 print(x, y) -- extra returns are discarded
+
+-- the following are the same:
+local function g(x)
+    return math.sin(x)
+end
+local g
+g = function(x)
+    return math.sin(x)
+end
+
+g = g(5)
+print(g)
+
+-- TABLES*
+-- can be used as many structures, e.g. arrays if we use integers as indexes
+-- using tables as dictionaries/maps
+-- string keys by default
+aTable = {key1 = "value1", key2 = false}
+
+-- js-like dot notation (works for strings)
+print(aTable.key1)
+aTable.newKey = "newValue" -- new key-value pair
+aTable.key2 = nil --remove key from the table (key 2 in this case)
+print(aTable.newKey)
+
+a = {} -- empty table
+-- create 1000 new entries
+for i = 1, 1000 do
+    a[i] = i * 2
+end
+print(a[9]) --> 18
+a["x"] = 10
+print(a["x"]) --> 10
+print(a["y"]) --> nil
+
+-- to clarify:
+--a["x"] and a.x ARE THE SAME
+print(a["x"])
+print(a.x)
+
+--Table iteration:
+for key, val in pairs(aTable) do
+    print(key, val)
+end
+
+function h(x)
+    print(x.key1)
+end
+h({key1 = "Table Declaration in Function call"})
+
+-- _G IS SPECIAL TABLE OF ALL GLOBALS
+print(_G["_G"] == _G)
+
+--tables as lists and arrays:
+-- List literals implicitly set up int keys:
+list = {"Im a string", 2.0, false, "c"}
+for i = 1, #list do -- "#" means "size" --> #list means "get me the size of list"
+    print(list[i]) -- INDEX START AT ONE
+end
+-- THIS IS STILL JUST A TABLE:
+for key, val in pairs(list) do
+    print(key, val)
+end
+
+--metatables and C++ like operation overloading
+fraction1 = {num = 1, denom = 2} --> this can be used, for example, to represent 1/2
+fraction2 = {num = 2, denom = 3}
+
+-- WE CANT CURRENTLY ADD FRACTION1 AND FRACTION2, SO:
+metafraction = {}
+function metafraction.__add(f1, f2)
+    sum = {}
+    sum.denom = f1.denom * f2.denom
+    sum.num = f1.num * f2.denom + f2.num * f1.denom
+    return sum
+end
+
+setmetatable(fraction1, metafraction)
+setmetatable(fraction2, metafraction)
+sum = fraction1 + fraction2
+print(sum.num .. " / " .. sum.denom)
